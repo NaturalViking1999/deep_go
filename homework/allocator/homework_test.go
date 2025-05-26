3eb49b2a48824dc0f4600bfec0a4d44677cfe92b
a.yaunberzinsh <a.yaunberzinsh@2gis.ru>
2025-05-26 20:45:37 +0300

    Homework 10 completed

---
+++ homework/allocator/homework_test.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"reflect"
+	"sort"
+	"testing"
+	"unsafe"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// go test -v homework_test.go
+
+func Defragment(memory []byte, pointers []unsafe.Pointer) {
+	sort.Slice(memory, func(i, j int) bool {
+		return memory[i] > memory[j]
+	})
+	for i := range pointers {
+		pointers[i] = unsafe.Pointer(&memory[i])
+	}
+}
+
+func TestDefragmentation(t *testing.T) {
+	var fragmentedMemory = []byte{
+		0xFF, 0x00, 0x00, 0x00,
+		0x00, 0xFF, 0x00, 0x00,
+		0x00, 0x00, 0xFF, 0x00,
+		0x00, 0x00, 0x00, 0xFF,
+	}
+
+	var fragmentedPointers = []unsafe.Pointer{
+		unsafe.Pointer(&fragmentedMemory[0]),
+		unsafe.Pointer(&fragmentedMemory[5]),
+		unsafe.Pointer(&fragmentedMemory[10]),
+		unsafe.Pointer(&fragmentedMemory[15]),
+	}
+
+	var defragmentedPointers = []unsafe.Pointer{
+		unsafe.Pointer(&fragmentedMemory[0]),
+		unsafe.Pointer(&fragmentedMemory[1]),
+		unsafe.Pointer(&fragmentedMemory[2]),
+		unsafe.Pointer(&fragmentedMemory[3]),
+	}
+
+	var defragmentedMemory = []byte{
+		0xFF, 0xFF, 0xFF, 0xFF,
+		0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00,
+	}
+
+	Defragment(fragmentedMemory, fragmentedPointers)
+	assert.True(t, reflect.DeepEqual(defragmentedMemory, fragmentedMemory))
+	assert.True(t, reflect.DeepEqual(defragmentedPointers, fragmentedPointers))
+}
